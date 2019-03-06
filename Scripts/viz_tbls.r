39014b6fca8b78ec2a156190b514eaa78756f884
Jamison Crawford <jrcrawfo@syr.edu>
2019-03-06 16:46:54 -0500

    Add files via upload

---
+++ Scripts/viz_tbls.r
@@ -0,0 +1,58 @@
+
+# Note: Run `reis_data_viz.r` prior to the following:
+
+library(readr)
+
+setwd("~/Projects/REIS/Master/Graphics Tables")
+
+write_csv(x = viz_expo_rc_hrs, "expo_rc_hrs_vztbl_13.csv")
+
+write_csv(x = viz_exp_sxrc_hrs, "expo_sxrc_hrs_vztbl_14.csv")
+
+write_csv(x = viz_expo_rc_grs, "expo_rc_grs_vztbl_15.csv")
+
+write_csv(x = tmp_expo_rc_viz, "tmp_expo_rc_vztbl_16.csv")
+
+write_csv(x = hw_sx_hrs, "hw_sx_hrs_vztbl_17.csv")
+
+write_csv(x = hw_sxrc_hrs_trade, "hw_sxrc_hrs_trade_vztbl_19.csv")
+
+write_csv(x = hw_sxrc_hrs_trade, "hw_sxrc_hrs_trade_vztbl_20.csv")
+
+write_csv(x = hw_wf_trades, "hw_wf_trades_vztbl_18.csv")
+
+write_csv(x = hw_hr_class, "hw_hrs_sxrc_class_vztbl_21.csv")
+
+write_csv(x = all_races_viz_tmp, "wf_rc_vztbl_22.csv")
+
+write_csv(x = all_races_viz_tmp, "wf_rc_vztbl_23.csv")
+
+write_csv(x = all_sxrc3, "wf_sxrc_24.csv")
+
+write_csv(x = gross_conclusion, "conclusion_grs_vztbl_25.csv")
+
+write_csv(x = hours_conclusion, "conclusion_hrs_vztbl_25.csv")
+
+write_csv(x = lv_dem_viz, "lv_dem_vztbl_8.csv")
+
+write_csv(x = lv_rc_con, "lv_rc_con_vztbl_9.csv")
+
+write_csv(x = sf_lv_county_stats, "sf_lv_county_stats_vztbl_11.csv")
+
+write_csv(x = lv_county_stats, "lv_county_stats_vztbl_10.csv")
+
+write_csv(x = lv_in_syr, "lv_in_syr_vztbl_12.csv")
+
+write_csv(x = hc_sxrc_narm, "hc_sxrc_narm_vztbl_1.csv")
+
+write_csv(x = viz_hc_rc_con, "hc_rc_con_vztbl_2.csv")
+
+write_csv(x = hc_rc_hrs, "hc_rc_hrs_vztbl_3.csv")
+
+write_csv(x = lv_grs_narm, "lv_grs_narm_vztbl_4.csv")
+
+write_csv(x = hc_county_stats, "hc_county_stats_vztbl_5.csv")
+
+write_csv(x = sf_hc_county, "sf_hc_county_vztbl_6.csv")
+
+write_csv(x = hc_in_city_stats, "hc_in_city_stats_vztbl_7.csv")
